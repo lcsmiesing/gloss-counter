2006f913d312e78e02d8d5646f2bb23e51239af1
Luuk Smiesing <lcsmiesing@gmail.com>
2022-11-07 15:57:26 +0100

    model dingen

--- src/Model.hs
+++ src/Model.hs
@@ -2,10 +2,6 @@
 --   which represent the state of the game
 module Model where
 
-data InfoToShow = ShowNothing
-                | ShowANumber Int
-                | ShowAChar   Char
-
 nO_SECS_BETWEEN_CYCLES :: Float
 nO_SECS_BETWEEN_CYCLES = 5
 
@@ -20,6 +16,32 @@ data GameState = GameState {
                 | GameOver {
                   points :: Int
                 }
+data Player = Player {
+                velocity :: Vector
+              , angle :: Vector
+              , position :: Point
+              }
+data Asteroid = Asteroid {
+                  velocity :: Vector
+                , position :: Point
+                , size :: Int
+                }
+data Enemy = Enemy {
+              velocity :: Vector
+            , position :: Point
+            }
+data Bullet = Bullet {
+                velocity :: Vector
+              , position :: Point
+              , fromEnemy :: Bool
+              }
+data Obstacle = Obstacle {
+                  position :: (Point, Point)
+                  width :: Int
+                }
+
+data Point = (Float, Float)
+data Vector = (Float, Float)
 
 
 
