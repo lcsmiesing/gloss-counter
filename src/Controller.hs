1f65103ac6eac94a30c40dc581b6002e86491d3a
lcsmiesing <50592960+lcsmiesing@users.noreply.github.com>
2022-11-12 15:38:41 +0100

    Update Controller.hs

--- src/Controller.hs
+++ src/Controller.hs
@@ -1,3 +1,5 @@
+{-# language NamedFieldPuns #-}
+
 -- | This module defines how the state changes
 --   in response to time and user input
 module Controller where
@@ -7,50 +9,159 @@ import Model
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import System.Random
+import Graphics.Gloss.Data.Vector
 
+runGame gstate@(GamePlay {player = p@(Player pvel pang ppos),
+                          bullets = b,
+                          asteroids = a, 
+                          obstacles = ob,
+                          enemies = em}) = gstate {player = updatePlayer p, bullets = map updateBull b, asteroids = newas, enemies = map updateEnemies em}
+  where
+    updateBull (Bullet vel pos f) = Bullet vel (vel .+ pos) f
+    get as@(Asteroid vel pos f s) = diff (vel,s) (map (coll as) ob)
+    gete em@(Enemy vel pos s) = diff (vel,s) (map (colle em) ob)
+    updateEnemies em@(Enemy vel pos s) = Enemy (rot ppos pos ((\(x,y) -> x) (gete em))) (edgeDetection gstate(vel .+ pos)) ((\(x,y) -> y) (gete em))
+    updateAsteroids as@(Asteroid vel pos f s) = Asteroid ((\(x,y) -> x) (get as))  (isHit (edgeDetection gstate (vel .+ pos)) f b) f ((\(x,y) -> y) (get as))
+    newas = map updateAsteroids a
+    updatePlayer (Player v a p) = Player (newVel v) a (newVel v .+ p)
+    newVel v | magV v > 10 = 10 .* (norm v)
+             | otherwise   = v
+
+--(isHit (edgeDetection gstate (vel .+ pos)) f b)
 -- | Handle one iteration of the game
 step :: Float -> GameState -> IO GameState
-step secs gstate
-  | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES
+step secs gstate@(GamePlay {isPaused = p})
+  | not p
   = -- We show a new random number
-    do randomNumber <- randomIO
-       let newNumber = abs randomNumber `mod` 10
-       return $ GameState (ShowANumber newNumber) 0
+    do return $ runGame gstate {elapsedTime = elapsedTime gstate + secs}
+  | p
+  =
+    do return gstate
   | otherwise
   = -- Just update the elapsed time
     return $ gstate { elapsedTime = elapsedTime gstate + secs }
 
--- | Handle user input
+edgeDetection :: GameState -> Point -> Point
+edgeDetection GamePlay {gameBorders = ((a,b),(c,d)), obstacles = ob} (x,y) 
+                              | x <= a-20 = (abs x, y)
+                              | x >= b+20 = (-x, y)
+                              | y <= c-20 = (x, abs y)
+                              | y >= d+20 = (x, -y)
+                              | otherwise = (x,y)
+edgeDetection GameOver {points} p = p
+
+--this function checks if an asteroid collides with a side of an obstacle
+--if it does, it changes the asteroids direction vector to "bounce" off the side of the obstacle
+--using a predefined reflection vector
+coll :: Asteroid -> Obstacle -> (Vector,String)
+coll (Asteroid v p@(x,y) _ f) (Obstacle _ _ _ (a,b,c,d)) | top && f /= "top" = (v .- ((2*dotproduct v (0,1)).*(0,1)),"top")
+                                                       | left && f/= "left" = (v .- ((2*dotproduct v (-1,0)).*(-1,0)),"left")
+                                                       | right && f/="right" = (v .- ((2*dotproduct v (1,0)).*(1,0)),"right")
+                                                       | bottom && f/="bottom" = (v .- ((2*dotproduct v (0,-1)).*(0,-1)),"bottom")
+                                                       | otherwise = (v,f)
+  where
+    top = distance a b p -- (0,1)
+    left = distance a c p --(-1,0)
+    right = distance b d p --(1,0)
+    bottom = distance c d p --(0,-1)
+
+--same as coll but for enemies  
+colle :: Enemy -> Obstacle -> (Vector,String)
+colle (Enemy v p f) (Obstacle _ _ _ (a,b,c,d)) | top && f /= "top" = (v .- ((2*dotproduct v (0,1)).*(0,1)),"top")
+                                                       | left && f/= "left" = (v .- ((2*dotproduct v (-1,0)).*(-1,0)),"left")
+                                                       | right && f/="right" = (v .- ((2*dotproduct v (1,0)).*(1,0)),"right")
+                                                       | bottom && f/="bottom" = (v .- ((2*dotproduct v (0,-1)).*(0,-1)),"bottom")
+                                                       | otherwise = (v,f)
+  where
+    top = distance a b p -- (0,1)
+    left = distance a c p --(-1,0)
+    right = distance b d p --(1,0)
+    bottom = distance c d p --(0,-1)
+
+
+diff :: (Vector,String) -> [(Vector,String)] -> (Vector,String)
+diff v [] = v
+diff v (x:xs) | v /= x = x
+              | otherwise = diff v xs
+
+isHit :: Point -> Float -> [Bullet] -> Point
+isHit p s b | mini < s = (-300,300) 
+            | otherwise = p
+  where
+    bulletMap = [pos | Bullet _ pos _ <- b]
+    m = map (disc s p) bulletMap
+    mini = minimum' m
+
+minimum' :: [Float] -> Float
+minimum' [] = 99999999999
+minimum' [i] = i
+minimum' (i:is) | i == (-1) = -1
+                | otherwise = min i (minimum' is)
+                        
 input :: Event -> GameState -> IO GameState
 input e gstate = return (inputKey e gstate)
 
 inputKey :: Event -> GameState -> GameState
-inputKey (EventKey (Char c) _ _ _) gstate
-  = case c of
-      'w' -> --iets jwz
-      'a' ->
-      's' ->
-      'd' ->
+inputKey (EventKey (Char 'p') Down _ _) gstate@(GamePlay {isPaused})
+    | isPaused  = gstate {isPaused = False}
+    | otherwise = gstate {isPaused = True}
 
-inputKey (EventKey (MouseButton LeftButton) Down MousePos)
-  = gstate@(GameState {player, bullets}) = gstate {bullets = bullets:newBullet}
+inputKey (EventKey (Char c) Down _ _) gstate@(GamePlay {player = Player vel ang pos})
+  = let direction = case c of 
+                      'w' -> (0, 5)
+                      'a' -> (-5, 0)
+                      's' -> (0, -5)
+                      'd' -> (5, 0)
+                      _   -> (0, 0)
+    in
+      gstate {player = Player ((vel .+ direction)) ang pos}
+        
+
+inputKey (EventKey (MouseButton LeftButton) Down _ clickPos)
+  gstate@(GamePlay {player = p@(Player _ _ posp), bullets, isPaused})
+    | isPaused  = gstate 
+    | otherwise = gstate {bullets = bullets ++ [newBullet]}
     where
-      newBullet = Bullet {velb = bulletVel posb, posb = player@posp, fromEnemy = False}
-      bulletVel p =  norm (p .- MousePos) .* 10
-      posb = --SDFHSDFSDKLFSLDJFLKSDJFSLKDJFSLKDFSDLKJF
+      newBullet = Bullet bulletVel posp False
+      bulletVel =  10 .* norm (clickPos .- posp)
+
+inputKey (EventMotion (x,y)) gstate@(GamePlay {player = (Player velp angle posp@(a,b))}) 
+  = gstate {player = Player velp (argV (x-a,y-b)) posp}
+
 inputKey _ gstate = gstate -- Otherwise keep the same
 
-.+ :: Point -> Point -> Point
-.+ (Point x y) (Point a b) = Point (x+a) (y+b)
+(.+) :: Point -> Point -> Point
+(x,y) .+ (a,b) = (x+a, y+b)
 
-.- :: Point -> Point -> Point
-.- (Point x y) (Point a b) = Point (x-a) (y-b)
+(.-) :: Point -> Point -> Point
+(x,y) .- (a,b) = (x-a, y-b)
 
-.* :: Vector -> Float -> Vector
-.* (Vector x y) s = Vector (s*x) (s*y)
+(.*) :: Float -> Vector -> Vector
+s .* (x,y) = (s*x,s*y)
+
+dotproduct :: Vector -> Vector -> Float
+dotproduct (x,y) (a,b) = x*a + y*b
 
 norm :: Vector -> Vector
-norm v@(Vector x y) = let m = magn v in (Vector x/m y/m)
+norm (x,y) = let m = magn (x,y) in (x/m,y/m)
 
 magn :: Vector -> Float
-magn (Vector x y) = sqrt(x**2 + y**2)
+magn (x,y) = sqrt(x**2 + y**2)
+
+rot :: Point -> Point -> Vector -> Vector
+rot p1 p2 vec = res
+    where
+      dirv = norm (p1 .- p2)
+      res = norm (vec .+ (0.05 .* dirv))
+
+disc :: Float -> Point -> Point -> Float
+disc size c p = abs(size - (dis c p))
+
+distance :: (Float,Float) -> (Float,Float) -> (Float,Float) -> Bool
+distance a b c = dis a c + dis c b > dis a b - 0.5  && dis a c + dis c b < dis a b + 0.5 
+
+dis :: Point -> Point -> Float
+dis (x1 , y1) (x2 , y2) = sqrt (x'*x' + y'*y')
+    where
+      x' = x1 - x2
+      y' = y1 - y2
