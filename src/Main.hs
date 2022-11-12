2bcb4fca04543758aca2d7574465a0203a7935bb
lcsmiesing <50592960+lcsmiesing@users.noreply.github.com>
2022-11-12 15:39:20 +0100

    Update Main.hs
    Hoe gaat het met jou? Met mij gaat het goed.
    Groetjes Luuk

--- src/Main.hs
+++ src/Main.hs
@@ -5,12 +5,31 @@ import Model
 import View
 
 import Graphics.Gloss.Interface.IO.Game
+import System.Random
 
 main :: IO ()
-main = playIO (InWindow "Best game 4ever" (600, 600) (0, 0)) -- Or FullScreen
+main = do
+    let aran = 6
+    let size = (600,600)
+    m <- readFile "obs.txt"
+    x <- randomList aran
+    y <- randomList aran
+    v <- randomList aran
+    w <- randomList aran
+    let l = lines m
+    playIO (InWindow "Best game 4ever" size (0, 0)) -- Or FullScreen
               black            -- Background color
-              10               -- Frames per second
-              initialState     -- Initial state
+              60               -- Frames per second
+              (initialState l x y v w (600,600))     -- Initial state
               view             -- View function
               input            -- Event function
               step             -- Step function
+
+
+
+randomList :: Int -> IO [Float]
+randomList 0 = return []
+randomList n = do
+  r  <- randomRIO(-1,1) :: IO Float
+  rs <- randomList (n-1)
+  return (r:rs) 
