06d3b71617156dc22541bc71743a066381962ec1
lcsmiesing <50592960+lcsmiesing@users.noreply.github.com>
2022-11-12 15:40:08 +0100

    Update View.hs

--- src/View.hs
+++ src/View.hs
@@ -4,12 +4,43 @@ module View where
 
 import Graphics.Gloss
 import Model
+import Controller
+import Graphics.Gloss.Data.Vector
 
-view :: GameState -> IO Picture
+view :: GameState  -> IO Picture
 view = return . viewPure
 
 viewPure :: GameState -> Picture
-viewPure gstate = case infoToShow gstate of
-  ShowNothing   -> blank
-  ShowANumber n -> color green (text (show n))
-  ShowAChar   c -> color green (text [c])
+viewPure (GamePlay {player = p, bullets = b, obstacles = ob, asteroids = as, enemies = em}) = 
+  pictures [speler p, kogel b, obs ob, ast as, ene em]
+  where
+    --speler (Player _ _ (x,y)) = color green (translate x y (polygon [(0,50), (-25,0), (25,0)]))
+    speler (Player _ angle (x,y)) = color green (translate x y (polygon (map (rotateV angle) [(-3,-5), (-5,0), (-3,5), (10,0)])))
+    kogel b = pictures [translate x y (color white (circle 2))
+                         | Bullet _ (x,y) _ <- b]
+    obs ob = pictures [color orange (translate x y (rectangleSolid w h)) 
+                         | Obstacle (x,y) w h d <- ob]
+    ast as = pictures ([color white (translate v w (circleSolid s))
+                         | Asteroid (x,y) (v,w) s f <- as] ++ [color white (line [posa,posa.+(50.*vela)])
+                         | Asteroid vela posa s f <- as])
+
+    ene em = pictures [color red (arrow [posa,posa.+(15.*norm vela)]) | Enemy vela posa s <- em]
+
+-- two functions below are taken from:
+-- https://github.com/ivanperez-keera/SoOSiM-ui/blob/master/src/Graphics/Gloss/AdvancedShapes/Arrows.hs
+arrow :: Path -> Picture
+arrow p 
+  | [p1, p2] <- p, p1 /= p2
+  , (v1, v2) <- calcArrowHeadVertex (p1, p2)
+  = color red $ Pictures [ line [p1, p2]
+                                , polygon [p2, v1, v2]
+                                ]
+arrow _ = blank
+
+calcArrowHeadVertex :: (Point, Point) -> (Point, Point)
+calcArrowHeadVertex ((start_x, start_y), (end_x, end_y)) = ((x1, y1), (x2, y2))
+  where angle = atan2 (end_y - start_y) (end_x - start_x)
+        x1 = end_x - 10 * cos(angle - 50)
+        y1 = end_y - 10 * sin(angle - 50)
+        x2 = end_x - 10 * cos(angle + 50)
+        y2 = end_y - 10 * sin(angle + 50)
